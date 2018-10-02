43e3d5ee59ce384380b054e748bc915724141918
Asperansa <asperansa@gmail.com>
2018-10-02 14:20:02 +0500

    Добавили компонент TodoList списка задач

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div class="app">
+    <TodoList></TodoList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TodoList,
   },
 };
 </script>
 
 <style>
-#app {
+.app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ul>
+      <li>To water flowers</li>
+      <li>Buy products</li>
+      <li>Pay the bills</li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+</script>
+
+<style scoped>
+</style>
